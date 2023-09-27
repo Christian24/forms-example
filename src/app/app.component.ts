f200789271cc6fd10ac57be0bb96bf54e42796d3
Christian Siebmanns <christian.siebmanns@hotmail.de>
2023-09-27 21:52:03 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+
+type pizzaGroupType = {name: FormControl<string |null>, cut: FormControl<boolean | null>};
+type addressGroupType = {street: FormControl<string | null>, houseNumber: FormControl<string | null>, postalCode: FormControl<number | null>, city: FormControl<string | null>}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+
+export class AppComponent implements OnInit{
   title = 'forms';
+  public pizzaGroup;
+  public sameAddress = false;
+
+  constructor(private formBuilder: FormBuilder) {
+
+
+  this.pizzaGroup = this.formBuilder.group({pizzas: this.formBuilder.array<FormGroup<pizzaGroupType>>([]),
+   billingAddress: this.createAddressFormGroup(), deliveryAddress: this.createAddressFormGroup(), sameAddress: new FormControl(false)})
+  }
+
+  protected createAddressFormGroup(): FormGroup<addressGroupType> {
+    return this.formBuilder.group({street: new FormControl<string>(''), houseNumber: new FormControl<string>(''), postalCode: new FormControl<number | null>(null), city: new FormControl<string>('')})
+  }
+
+  public addPizza() {
+   const pizza = this.formBuilder.group({name: new FormControl(''), cut: new FormControl(false)});
+   this.pizzaGroup.controls.pizzas.push(pizza);
+  }
+  public deletePizza(index: number) {
+    this.pizzaGroup.controls.pizzas.removeAt(index);
+  }
+
+  public getPizzas() {
+    return this.pizzaGroup.controls.pizzas.controls;
+  }
+
+  protected readonly JSON = JSON;
+
+  ngOnInit(): void {
+    this.pizzaGroup.controls.sameAddress.valueChanges.subscribe((value) => {
+      if (value !== null) {
+        this.sameAddress = value;
+      }
+      if (value) {
+        this.pizzaGroup.controls.billingAddress.reset();
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
